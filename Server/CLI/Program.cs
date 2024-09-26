0f4a308ddaaf93f521621acf52143e0170b6f74d
CatalinaTonu <catalinatonu9@gmail.com>
2024-09-26 11:08:46 +0200

    new changes

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,15 +1,15 @@
 ﻿using CLI;
 using CLI.UI;
 
-using InMemoryRepositories;
+using FileRepositories;
 using RepositoryContracts;
 
 Console.WriteLine("Starting CLI app...");
 // Repositories are instantiated here. Notice this version uses the FileRepositories instead of the InMemoryRepositories. That doesn't matter for you.
 
-IUserRepository userRepository = new UserInMemoryRepository(); // old: UserInMemoryRepository();
-ICommentRepository commentRepository = new CommentInMemoryRepository(); // old: CommentInMemoryRepository();
-IPostRepository postRepository = new PostInMemoryRepository(); // old:  PostInMemoryRepository();
+IUserRepository userRepository = new UserFileRepository(); // old: UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentFileRepository(); // old: CommentInMemoryRepository();
+IPostRepository postRepository = new PostFileRepository(); // old:  PostInMemoryRepository();
 
 CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
 await cliApp.StartAsync(); // everything is async, so we await it.
